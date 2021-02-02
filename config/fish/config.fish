c4b9514a1790175efca666c029790bf97ef097f3
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2021-02-01 20:51:03 -0500

    make fish and rvm work

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,13 @@
-set -x PATH ~/bin/ $PATH 
-set -U EDITOR nvim
-
 # since these aliases are shared with bash, not converting these to functions
 if test -e ~/.aliases
   source ~/.aliases
 end
 
-if test -e ~/.environment
-  source ~/.environment
+if test -e $HOME/.config/fish/environment
+  source $HOME/.config/fish/environment
 end
+
+fish_vi_key_bindings
+set -e fish_greeting # remove default greeting
+
+rvm default
