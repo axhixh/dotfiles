bd2e9cce62d8bce06f57a5d712a895fb13cc905e
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2022-02-25 06:45:08 -0500

    switching to lua for neovim

---
+++ config/nvim/lua/axhixh/options.lua
@@ -0,0 +1,29 @@
+local o = vim.opt
+
+o.backup = false -- create a backup file
+o.swapfile = false
+o.clipboard = "unnamedplus" -- allow access to system clipboard
+o.cmdheight = 2 -- extra space for notification
+o.fileencoding = "utf-8"
+o.mouse = "a" -- enable mouse in all modes
+o.wrap = false
+o.splitbelow = true
+o.splitright = true
+
+o.hlsearch = true -- highlight search results
+o.incsearch = true
+o.ignorecase = true
+o.smartcase = true
+
+o.smartindent = true
+o.autoindent = true
+
+o.smarttab = true
+o.tabstop = 4
+o.expandtab = true
+o.shiftwidth = 4
+
+o.number = true
+o.relativenumber = true
+o.colorcolumn = "65,80,120"
+o.showmode = false
