a59d98d83d631d9fb6b7588417dd957ff05bef06
Ashish Shrestha <96478+axhixh@users.noreply.github.com>
2022-02-25 23:09:48 -0500

    nvim treesitter

---
+++ config/nvim/lua/axhixh/treesitter.lua
@@ -0,0 +1,19 @@
+local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+    return
+end
+
+treesitter.setup {
+    ensure_installed = "maintained",
+    sync_install = false,
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
+    },
+    highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+    },
+}
