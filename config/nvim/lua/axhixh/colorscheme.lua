0950781b028423083b2e04c8615680e1d8bc1b50
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2023-09-01 21:00:03 -0400

    no longer using neovim

--- config/nvim/lua/axhixh/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-local colorscheme = "deus"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-    vim.notify("color scheme " .. colorscheme .. " not found!")
-    return
-end
