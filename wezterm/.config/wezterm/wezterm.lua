bae91ba43bd6e9ec627c774735fbeb0abba1e4d3
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2024-08-12 10:45:51 -0400

    integrate tabs into title bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -6,7 +6,12 @@ if wezterm.config_builder then
 end
 
 config.font_size = 16.0
-config.font = wezterm.font('VictorMono Nerd Font Mono', 
-  { weight = 'Thin' })
- 
+-- config.font = wezterm.font('VictorMono Nerd Font Mono', 
+--  { weight = 'Thin' })
+config.font = wezterm.font('CommitMonoaxhixh') 
+
+config.color_scheme = 'Atelierheath (light) (terminal.sexy)'
+
+config.window_decorations = 'INTEGRATED_BUTTONS'
+
 return config
