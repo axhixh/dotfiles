8c76ca6c20b2a8678676292eefbd491088ec501f
Ashish Shrestha <896478+axhixh@users.noreply.github.com>
2025-05-05 21:23:53 -0400

    update opam

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,5 +14,10 @@ end
 fish_vi_key_bindings
 set -e fish_greeting # remove default greeting
 
-# opam configuration
-source /Users/ashrestha/.opam/opam-init/init.fish >/dev/null 2>/dev/null; or true
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '$HOME/.opam/opam-init/init.fish' && source '$HOME/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
+# END opam configuration
